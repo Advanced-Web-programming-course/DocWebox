3352b79364cb5d992a2dae03b26a4d90b76aa6db
Nektatios Papagalakis <nektarios00007@gmail.com>
2022-11-05 21:43:02 +0200

    .

--- search doctor/search_doc.php
+++ search doctor/search_doc.php
@@ -5,13 +5,16 @@ include "search_doc_functions.php";
 <html>
 <head>
     <link rel="stylesheet" href="search_doctor_doc_element.css">
+    <link rel="stylesheet" href="../search_bar.css">
 </head>
 <title>doctor</title>
 <script src="https://kit.fontawesome.com/d2c306d566.js" crossorigin="anonymous"></script>
 <body>
 
+<div style = "margin-bottom: 25px;">
+        <?php include "C:\\xampp\\htdocs\\DocWebox\\php_nektatios\\mainPage\\searchBar.php";?>
+</div>
 <?php
-
 echo add_doctor("Μαρια Παπαδοπούλου","Παθολόγος","Λαζαράκη 33,Γλυφάδα","50",0);
 echo add_doctor("Άννα Παπαδοπούλου","Ουρολόγος","Καποδίστρια 9, Ωραιόκαστο","100",3);?>
 
