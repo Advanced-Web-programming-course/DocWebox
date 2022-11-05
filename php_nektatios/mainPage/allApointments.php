2196c9a6bb713cb82b14e41fff35708c24067057
ElisavetAmpatzidou <elisabet.ampatzidou@gmail.com>
2022-11-05 19:22:59 +0200

    feat:create allApointments searchBar and upcomingApointments Components

---
+++ php_nektatios/mainPage/allApointments.php
@@ -0,0 +1,25 @@
+<div class='all-apointments'>
+    <div class=' title'>Όλα τα ραντεβού</div>
+    <section>
+        <header>
+            <div class="col">Ονοματεπώνυμο Γιατρού</div>
+            <div class="col">Υπηρεσία</div>
+            <div class="col">Τιμή</div>
+            <div class="col">Ημερομηνία</div>
+            <div class="col">Βαθμολογία</div>
+            <div class="col"></div>
+        </header>
+        <div class="row">
+            <div class="col">Δρ. Παπαιωάννου</div>
+            <div class="col">Απλή Επίσκεψη</div>
+            <div class="col">€25</div>
+            <div class="col">27/10/2022 09:30</div>
+            <div class="col">asteria</div>
+            <div class="col">
+                <a href="">
+                    icon tel
+                </a>
+            </div>
+        </div>
+    </section>
+</div>
