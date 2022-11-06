d30907494fabf9a7fa5ded48d1858f10ec594c42
evitadasy <dasyevita@gmail.com>
2022-11-06 18:50:43 +0200

    login page front-end

---
+++ components/loginForm.php
@@ -0,0 +1,38 @@
+<!-- login form -->
+
+
+<div class='container'>
+
+   <div class='upbtns'>
+             <button class='sign-in'>Σύνδεση</button>
+             <button class='register'>Εγγραφή</button>
+   </div>
+
+   <form class='login' action='' method='post'>
+
+         <div class='form-field'>
+            <h1 class='title'>Καλωσήρθατε</h1>
+            <h4 class='subtitle'>Συνδεθείτε για να δείτε τα ραντεβού σας</h4>
+
+            <div>
+               <label class='lemail' for='login-email'>E-mail</label><br>
+               <input id='login-email' class="form-input" type='email'  required>
+            </div>
+
+            <div>
+               <label class='lpass' for='login-pass'>Password</label><br>
+               <input id='login-pass' class="form-input" type='password'  required>
+            </div>
+            
+            <a class='forgot-pass' href="#">Ξέχασα τον κωδικό μου</a>
+            <button class='logbtn'>Login</button>
+            <div class='sign-as'>
+               <a  href="#">Είσοδος ως XXXX</a>
+            </div>
+         </div>
+
+         
+
+   </form>
+
+</div>
