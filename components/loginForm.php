e1ab5c23ffad7af108f77e5e6dd20ad14cb154e5
evitadasy <dasyevita@gmail.com>
2022-11-06 19:50:18 +0200

    fix class names and btns

--- components/loginForm.php
+++ components/loginForm.php
@@ -14,18 +14,18 @@
 
             <div>
                <label class='lemail' for='login-email'>E-mail</label><br>
-               <input id='login-email' class="form-input" type='email'  required>
-            </div>
-
-            <div>
-               <label class='lpass' for='login-pass'>Password</label><br>
-               <input id='login-pass' class="form-input" type='password'  required>
+               <input id='login-email' type='email' required autofocus>
             </div>
             
+            <div>
+               <label class='lpass' for='login-pass'>Password</label><br>
+               <input id='login-pass' type='password' required>
+            </div>     
+            
             <a class='forgot-pass' href="#">Ξέχασα τον κωδικό μου</a>
-            <button class='logbtn'>Login</button>
+            <button class='loginbtn'>Σύνδεση</button>
             <div class='sign-as'>
-               <a  href="#">Είσοδος ως XXXX</a>
+              <a  href="#">Είσοδος ως XXXX</a>
             </div>
          </div>
 
--- components/registerForm.php
+++ components/registerForm.php
@@ -6,7 +6,7 @@
              <button class='register2'>Εγγραφή</button>
    </div>
 
-   <form class='login' action='' method='post'>
+   <form class='register' action='' method='post'>
 
          <div class='form-field'>
             <h1 class='title'>Καλωσήρθατε</h1>
@@ -14,21 +14,21 @@
 
             <div>
                <label class='lname' for='login-name'>Ονοματεπώνυμο</label><br>
-               <input id='login-name' class="form-input" type='text'  required>
+               <input id='login-name' type='text' required autofocus>
             </div>
 
             <div>
                <label class='lemail' for='login-email'>E-mail</label><br>
-               <input id='login-email' class="form-input" type='email'  required>
+               <input id='login-email' type='email' required>
             </div>
 
             <div>
                <label class='lpass' for='login-pass'>Password</label><br>
-               <input id='login-pass' class="form-input" type='password'  required>
+               <input id='login-pass' type='password'  required>
             </div>
             
             <a class='forgot-pass' href="#">Ξέχασα τον κωδικό μου</a>
-            <button class='logbtn'>Εγγραφή</button>
+            <button class='registerbtn'>Εγγραφή</button>
          </div>
 
          
