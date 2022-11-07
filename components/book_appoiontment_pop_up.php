8f3f6acc7cff7a1a7b0d79f8324e2d7c41830a87
evitadasy <dasyevita@gmail.com>
2022-11-08 00:45:31 +0200

    book appoiontment pop up front end, later gonna add CALENDAR

---
+++ components/book_appoiontment_pop_up.php
@@ -0,0 +1,25 @@
+<!-- review popUp form -->
+<link rel="stylesheet" href="../css/pop_up.css">
+
+<div class='popUp'>
+    <form class='book-appointment-form' action='' method='post'>  
+
+        <div class='title'>Υπηρεσία</div>
+        <div class='subtitle' id='simple-visit'>Απλή Επίσκεψη   €30</div>  
+        <div class='title'>Επιλέξτε ημέρα και ώρα του ραντεβού</div>         
+       
+        <!-- ΤΗΑ ΒΑΛΟΥΜΕ CALENDAR ΕΔΩ -->
+        <div class='comment'>
+            <textarea class='dtext' id='description-text' type='text' placeholder="......"></textarea>
+        </div>
+
+        <div class='bottombtns'>
+            <button class='cancel' id='dcancelbtn'>Ακύρωση</button>
+            <button class='continue' id='continue'>Συνέχεια</button>
+        </div>
+
+    </form>
+
+</div>
+
+
