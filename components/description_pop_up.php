1ae91aa2a5092061a05beffa84d11d66b8dda1cd
evitadasy <dasyevita@gmail.com>
2022-11-08 00:01:32 +0200

    description pop up front- end

---
+++ components/description_pop_up.php
@@ -0,0 +1,23 @@
+<!-- review popUp form -->
+<head>
+    <link rel="stylesheet" href="../css/pop_up.css">
+</head>
+<div class='popUp'>
+    <form class='description-form' action='' method='post'>  
+
+        <div class='title'>Η περιγραφή μου</div>        
+
+        <div class='comment'>
+            <textarea class='dtext' id='description-text' type='text' placeholder="Γράψτε περισσότερα για εσάς......"></textarea>
+        </div>
+
+        <div class='bottombtns'>
+            <button class='cancel' id='dcancelbtn'>Ακύρωση</button>
+            <button class='save' id='save'>Αποθήκευση</button>
+        </div>
+
+    </form>
+
+</div>
+
+
