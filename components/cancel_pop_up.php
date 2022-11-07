24c179f0cd82d751c76aee348b2f0fb124361e7b
evitadasy <dasyevita@gmail.com>
2022-11-07 19:06:04 +0200

    cancel pop up front end

---
+++ components/cancel_pop_up.php
@@ -0,0 +1,19 @@
+<!-- review popUp form -->
+<head>
+    <link rel="stylesheet" href="../css/pop_up.css">
+</head>
+
+<div class='popUp'>
+    <form class='cancel-form' action=''>  
+
+        <div class='title' id='cancel-title'>Επιθυμείς να ακυρώσεις το ραντεβού σου;</div>
+
+        <div class='bottombtns'>
+            <button class='cancel' id='ccancelbtn'>Ακύρωση</button>
+            <button class='confirm' id='confirmbtn'>Επιβεβαίωση</button>
+        </div>
+    </form>
+
+</div>
+
+
