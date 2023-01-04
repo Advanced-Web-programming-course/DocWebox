4fc49b522205bbe5578cc3c21fb7ad32d9fe6b3d
Aravella Lousta <73483257+aravellalousta@users.noreply.github.com>
2023-01-04 23:04:02 +0200

    added datepicker to book appointment modal
    will add a timepicker next

--- components/modals/book_appointment_modal.php
+++ components/modals/book_appointment_modal.php
@@ -1,144 +1,72 @@
 <!-- book appointment modal -->
-<div class="modal fade" id="bookAppointmentModal" tabindex="-1" aria-labelledby="bookAppointmentModalLabel" aria-hidden="true">
-  <div class="modal-dialog modalCenter modal-lg">
-    <div class="modal-content">
-        <form class='book-appointment-form' action='' method='post'>
-            <div class="modal-body">
-                <div class='btitle'>Υπηρεσία</div>
-                
-                <!-- PREPEI NA ALLAZEI H YPHRESIA ANALOGWS TI DIALEGEI O XRISTIS-->
-                <div class='bsubtitle' id='simple-visit'>Απλή Επίσκεψη €30</div>
-                <div class='btitle'>Επιλέξτε ημέρα και ώρα του ραντεβού</div> <br />
-                <div class='comment'>
-            <div class='calendararea'>
-                <a href="#" role="button">
-                    <img src="../images/chevron-left.png" alt="" width="10" height="10">
-                </a>
-                <div class="appointment-option">
-                    <div class="appointment-day">
-                        Δευτέρα 07/11
-                    </div>
-                    <div class="appointment-hours">
-                        <div class="appointment-hour">
-                            <a>
-                                18:00
-                            </a>
-                        </div>
-                        <div class="appointment-hour">
-                            <a>
-                                18:30
-                            </a>
-                        </div>
-                        <div class="appointment-hour">
-                            <a>
-                                19:00
-                            </a>
-                        </div>
-                    </div>
-                </div>
+<div class="modal fade" id="bookAppointmentModal" tabindex="-1" aria-labelledby="bookAppointmentModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog modalCenter modal-lg">
+        <div class="modal-content">
+            <form class='book-appointment-form' action='' method='post'>
+                <div class="modal-body">
+                    <div class='btitle'>Υπηρεσία</div>
 
-                <div class="appointment-option">
-                    <div class="appointment-day">
-                        Τρίτη 08/11
-                    </div>
-                    <div class="appointment-hours">
-                        <div class="appointment-hour">
-                            <a>
-                                18:00
-                            </a>
-                        </div>
-                        <div class="appointment-hour">
-                            <a>
-                                18:30
-                            </a>
-                        </div>
-                        <div class="appointment-hour">
-                            <a>
-                                19:00
-                            </a>
-                        </div>
-                    </div>
-                </div>
+                    <!-- PREPEI NA ALLAZEI H YPHRESIA ANALOGWS TI DIALEGEI O XRISTIS-->
+                    <div class='bsubtitle' id='simple-visit'>Απλή Επίσκεψη €30</div>
+                    <div class='btitle'>Επιλέξτε ημέρα και ώρα του ραντεβού</div> <br />
+                    <div class='comment'>
+                        <div class='calendararea'>
+                            <section class="container">
+                                <form>
+                                    <div class="row form-group">
+                                        <label for="date" class="col-sm-1 col-form-label">Ημέρα</label>
+                                        <div class="col-sm-4">
+                                            <div class="input-group date" id="datepicker">
+                                                <input type="text" class="form-control">
+                                                <span class="input-group-append">
+                                                    <span class="input-group-text bg-white d-block">
+                                                        <i class="fa fa-calendar"></i>
+                                                    </span>
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
+                            </section>
 
-                <div class="appointment-option">
-                    <div class="appointment-day">
-                        Τετάρτη 09/11
-                    </div>
-                    <div class="appointment-hours">
-                        <div class="appointment-hour">
-                            <a>
-                                18:00
-                            </a>
-                        </div>
-                        <div class="appointment-hour">
-                            <a>
-                                18:30
-                            </a>
-                        </div>
-                        <div class="appointment-hour">
-                            <a>
-                                19:00
-                            </a>
+                            <section class="container">
+                                <form>
+                                    <div class="row form-group">
+                                        <label for="date" class="col-sm-1 col-form-label">Ώρα</label>
+                                        <div class="col-sm-4">
+                                            <div class="input-group date" id="timepicker">
+                                                <input type="text" class="form-control">
+                                                <span class="input-group-append">
+                                                    <span class="input-group-text bg-white d-block">
+                                                        <i class="fa-regular fa-clock"></i>
+                                                    </span>
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
+                            </section>
                         </div>
                     </div>
-                </div>
 
-                <div class="appointment-option">
-                    <div class="appointment-day">
-                        Πέμπτη 10/11
-                    </div>
-                    <div class="appointment-hours">
-                        <div class="appointment-hour">
-                            <a>
-                                18:00
-                            </a>
-                        </div>
-                        <div class="appointment-hour">
-                            <a>
-                                18:30
-                            </a>
-                        </div>
-                        <div class="appointment-hour">
-                            <a>
-                                19:00
-                            </a>
-                        </div>
-                    </div>
-                </div>
+                    <script type="text/javascript">
+                        $(function () {
+                            $('#datepicker').datepicker();
+                        });
+                    </script>
 
-                <div class="appointment-option">
-                    <div class="appointment-day">
-                        Παρασκευή 11/11
-                    </div>
-                    <div class="appointment-hours">
-                        <div class="appointment-hour">
-                            <a>
-                                18:00
-                            </a>
-                        </div>
-                        <div class="appointment-hour">
-                            <a>
-                                18:30
-                            </a>
-                        </div>
-                        <div class="appointment-hour">
-                            <a>
-                                19:00
-                            </a>
-                        </div>
-                    </div>
+                    <script type="text/javascript">
+                        $(function () {
+                            $('#timepicker').Timepicker(pickerInline, { format12: true, inline: true });
+                        });
+                    </script>
                 </div>
-                <a href="#" role="button">
-                    <img src="../images/chevron-right.png" alt="" width="10" height="10">
-                </a>
-            </div>
+                <div class="modal-footer">
+                    <button type="button" class='cancel' id='dcancelbtn' data-bs-dismiss="modal">Ακύρωση</button>
+                    <button type="submit" class='continue' id='continue'>Συνέχεια</button>
+                </div>
+            </form>
         </div>
-            </div>            
-            <div class="modal-footer">
-                <button type="button" class='cancel' id='dcancelbtn' data-bs-dismiss="modal">Ακύρωση</button>
-                <button type="submit" class='continue' id='continue'>Συνέχεια</button>
-            </div>
-        </form>
     </div>
-  </div>
 </div>
--- pages/doctor_selected_page.php
+++ pages/doctor_selected_page.php
@@ -17,15 +17,24 @@ echo $_GET['doc_id'];
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/doctor_selected/doctor_selected.css">
     <link rel="stylesheet" href="../css/doctor_selected/all_services.css">
-    <script src="https://kit.fontawesome.com/d2c306d566.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <link rel="stylesheet" href="../css/modals.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+    <script
+        src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
+    <script src="https://kit.fontawesome.com/d2c306d566.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../css/modals.css">
 
 </head>
 
 <body>
-    
+
     <?php
     include "../components/header.php";
     display_default_header($logged_user['full_name']);
@@ -34,7 +43,9 @@ echo $_GET['doc_id'];
     include "../components/all_services.php";
     ?>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
 
 </body>
 
