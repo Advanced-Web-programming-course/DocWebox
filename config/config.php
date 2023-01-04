f7f6a7f0d4669ec3ad2ba0f8a8cb6922694493a2
Aravella Lousta <73483257+aravellalousta@users.noreply.github.com>
2023-01-04 23:09:14 +0200

    cleaning up code

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 // Database Credentials
-define("DB_HOST","localhost");
-define("DB_DATABASE_NAME","docwebox");
-define("DB_USERNAME","root");
-define("DB_PASSWORD","");
+define("DB_HOST", "localhost");
+define("DB_DATABASE_NAME", "docwebox");
+define("DB_USERNAME", "root");
+define("DB_PASSWORD", "root");
 ?>
--- pages/doctor_selected_page.php
+++ pages/doctor_selected_page.php
@@ -17,17 +17,20 @@ echo $_GET['doc_id'];
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/doctor_selected/doctor_selected.css">
     <link rel="stylesheet" href="../css/doctor_selected/all_services.css">
-    <link rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
+    <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Bootstrap Datepicker -->
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script
         src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
+
     <script src="https://kit.fontawesome.com/d2c306d566.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../css/modals.css">
 
