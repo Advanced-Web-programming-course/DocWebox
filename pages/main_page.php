82816e1aa31de7bc66290b469da41ca487bd6b4f
Aravella Lousta <73483257+aravellalousta@users.noreply.github.com>
2023-01-15 18:27:31 +0200

    fixed css of main page

--- pages/main_page.php
+++ pages/main_page.php
@@ -64,7 +64,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
-    </script>
+        </script>
 
 </body>
 
