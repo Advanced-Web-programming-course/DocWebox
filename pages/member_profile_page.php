2593626c2987dcd6dab119a2c20387c46f41ef97
Aravella Lousta <73483257+aravellalousta@users.noreply.github.com>
2023-01-20 22:49:37 +0200

    member profile page css fixes

--- pages/member_profile_page.php
+++ pages/member_profile_page.php
@@ -34,7 +34,8 @@ if (isset($_GET['patient_id']) && !empty($_GET['patient_id']) && is_numeric($_GE
     <link rel="stylesheet" href="../css/edit_profile_section.css">
 
     <!-- Bootstrap, Ajax -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
@@ -60,7 +61,7 @@ if (isset($_GET['patient_id']) && !empty($_GET['patient_id']) && is_numeric($_GE
 
     ?>
     <script>
-        document.getElementById('edit-button').addEventListener('click', function(e) {
+        document.getElementById('edit-button').addEventListener('click', function (e) {
             openEditForm();
         });
 
